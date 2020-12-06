c716256d0d2c3519af10d070ad403e20d1887b20
Pontus <pontuspersson@outlook.com>
2020-12-06 18:39:21 +0100

    v6.1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('client'));
+app.get(express.static('client'));
 
 server.listen(PORT, function () {
   console.log('Chat server running');
